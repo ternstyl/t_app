8dff4102d9a4416f96fddf98db53cfb614631253
Marie Veverka <mlvsquirrel@yahoo.com>
2014-06-08 15:14:21 -0700

    Add a full_title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -10,9 +10,14 @@ describe "Static pages" do
       expect(page).to have_content('Ternstyl')
     end
      
-     it "should have the title 'Home'" do
+     it "should have the base title" do
       visit '/static_pages/home'
-      expect(page).to have_title("#{base_title} | Home")
+      expect(page).to have_title("#{base_title}")
+    end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      expect(page).not_to have_title('| Home')
     end
   end
 
@@ -88,9 +93,9 @@ describe "Static pages" do
       expect(page).to have_content('Terms & Conditions')
     end
 
-     it "should have the title 'Terms & Conditions'" do
+     it "should have the title 'Terms and Conditions'" do
       visit '/static_pages/terms'
-      expect(page).to have_title("#{base_title} | Terms & Conditions")
+      expect(page).to have_title("#{base_title} | Terms and Conditions")
     end
   end
 end
