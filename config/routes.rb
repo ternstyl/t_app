4b7d7de6a111676b8aaf16687d63564b95314eef
Marie Veverka <mlvsquirrel@yahoo.com>
2014-06-08 23:30:18 -0700

    Filling in layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 TApp::Application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/help"
-  get "static_pages/about"
-  get "static_pages/contact"
-  get "static_pages/privacy"
-  get "static_pages/terms"
-  get "static_pages/membership"
+  root  'static_pages#home'
+  match '/help',       to: 'static_pages#help',       via: 'get'
+  match '/about',      to: 'static_pages#about',      via: 'get'
+  match '/contact',    to: 'static_pages#contact',    via: 'get'
+  match '/privacy',    to: 'static_pages#privacy',    via: 'get'
+  match '/terms',      to: 'static_pages#terms',      via: 'get'
+  match '/membership', to: 'static_pages#membership', via: 'get'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,100 +2,56 @@ require 'spec_helper'
 
 describe "Static pages" do
 
-  let(:base_title) { "Ternstyl" }
+  subject { page }
 
   describe "Home page" do
-    it "should have the content 'Ternstyl'" do
-      visit '/static_pages/home'
-      expect(page).to have_content('Ternstyl')
-    end
-     
-     it "should have the base title" do
-      visit '/static_pages/home'
-      expect(page).to have_title("#{base_title}")
-    end
+    before { visit root_path }
 
-    it "should not have a custom page title" do
-      visit '/static_pages/home'
-      expect(page).not_to have_title('| Home')
-    end
-  end
-
-  describe "About page" do
-
-    it "should have the content 'About Ternstyl'" do
-      visit '/static_pages/about'
-      expect(page).to have_content('About Ternstyl')
-    end
-
-     it "should have the title 'About Ternstyl'" do
-      visit '/static_pages/about'
-      expect(page).to have_title("#{base_title} | About Ternstyl")
-    end
-  end
-
-  describe "Contact page" do
-
-    it "should have the content 'Contact'" do
-      visit '/static_pages/contact'
-      expect(page).to have_content('Contact')
-    end
-
-     it "should have the title 'Contact'" do
-      visit '/static_pages/contact'
-      expect(page).to have_title("#{base_title} | Contact")
-    end
+    it { should have_content('Ternstyl') }
+    it { should have_title(full_title('')) }
+    it { should_not have_title('| Home') }
   end
 
   describe "Help page" do
+    before { visit help_path }
 
-    it "should have the content 'Help'" do
-      visit '/static_pages/help'
-      expect(page).to have_content('Help')
-    end
-
-     it "should have the title 'Help'" do
-      visit '/static_pages/help'
-      expect(page).to have_title("#{base_title} | Help")
-    end
+    it { should have_content('Help') }
+    it { should have_title(full_title('Help')) }
   end
 
-  describe "Membership page" do
+  describe "About page" do
+    before { visit about_path }
 
-    it "should have the content 'Membership Agreement'" do
-      visit '/static_pages/membership'
-      expect(page).to have_content('Membership Agreement')
-    end
+    it { should have_content('About') }
+    it { should have_title(full_title('About Ternstyl')) }
+  end
 
-     it "should have the title 'Membership Agreement'" do
-      visit '/static_pages/membership'
-      expect(page).to have_title("#{base_title} | Membership Agreement")
-    end
+  describe "Contact page" do
+    before { visit contact_path }
+
+    it { should have_content('Contact') }
+    it { should have_title(full_title('Contact')) }
+  end
+
+    describe "Membership page" do
+    before { visit membership_path }
+
+    it { should have_content('Membership') }
+    it { should have_title(full_title('Membership Agreement')) }
   end
 
   describe "Privacy page" do
+    before { visit privacy_path }
 
-    it "should have the content 'Privacy Policy'" do
-      visit '/static_pages/privacy'
-      expect(page).to have_content('Privacy Policy')
-    end
-
-     it "should have the title 'Privacy Policy'" do
-      visit '/static_pages/privacy'
-      expect(page).to have_title("#{base_title} | Privacy Policy")
-    end
+    it { should have_content('Privacy') }
+    it { should have_title(full_title('Privacy Policy')) }
   end
 
   describe "Terms page" do
+    before { visit terms_path }
 
-    it "should have the content 'Terms & Conditions'" do
-      visit '/static_pages/terms'
-      expect(page).to have_content('Terms & Conditions')
-    end
-
-     it "should have the title 'Terms and Conditions'" do
-      visit '/static_pages/terms'
-      expect(page).to have_title("#{base_title} | Terms and Conditions")
-    end
+    it { should have_content('Terms') }
+    it { should have_title(full_title('Terms and Conditions')) }
   end
 end
+
