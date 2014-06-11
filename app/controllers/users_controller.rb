4fce3521ebaf07608eec60d2ace274d8e566a3a2
Marie Veverka <mlvsquirrel@yahoo.com>
2014-06-10 21:08:23 -0700

    Add basic user authentication and page protection

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user, only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
 
   def show
     @user = User.find(params[:id])
@@ -34,11 +36,25 @@ class UsersController < ApplicationController
   end
 
 
-  private
+   private
 
-    def user_params
+      def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
+    end
+
+    # Before filters
+
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
     end
 end
 
